7e7581e4aec963bc7aa3cdac692b5607836ec2af
Mateo Wallace <mateo.t.wallace@gmail.com>
2024-08-11 17:06:24 -0400

    production build

---
+++ README.md
@@ -0,0 +1,89 @@
+<p id="readme-top"></p>
+
+[![License Shield](https://img.shields.io/badge/License-MIT-success?style=for-the-badge)](./LICENSE) [![LinkedIn Shield](https://img.shields.io/badge/LinkedIn-555555?style=for-the-badge&logo=linkedin)](https://www.linkedin.com/in/mateo-wallace/)
+
+<!-- TITLE -->
+
+# React Portfolio - Mateo Wallace
+
+<!-- DESCRIPTION -->
+
+## Description
+
+Web Developer Portfolio built using React. Consists of a home, education, experience, projects, and contact page.
+
+While building this project I learned to:
+
+- Deploy a React App without a back end using [GitHub Pages](https://create-react-app.dev/docs/deployment/#github-pages)
+- Use [Iconify](https://iconify.design/) to display various tech related icons
+
+<!-- BUILT WITH -->
+
+### Built With
+
+[![REACT Shield](https://img.shields.io/badge/React-222222?&style=for-the-badge&logo=react)](https://reactjs.org/) [![Node.js Shield](https://img.shields.io/badge/Node.js-339933?&style=for-the-badge&logo=node.js&logoColor=white)](https://nodejs.org/en/) [![JavaScript Shield](https://img.shields.io/badge/JavaScript-F7DF1E?&style=for-the-badge&logo=javascript&logoColor=272727)](https://developer.mozilla.org/en-US/docs/Web/JavaScript) [![CSS Shield](https://img.shields.io/badge/CSS-1572B6?&style=for-the-badge&logo=css3&logoColor=white)](https://developer.mozilla.org/en-US/docs/Web/CSS) [![HTML Shield](https://img.shields.io/badge/HTML5-E34F26?&style=for-the-badge&logo=html5&logoColor=white)](https://developer.mozilla.org/en-US/docs/Glossary/HTML5)
+
+<!-- TABLE OF CONTENTS -->
+
+## Table of Contents
+
+- [Deployed Site](#deployed-site)
+- [Deployment Instructions](#deployment-instructions)
+- [License](#license)
+- [Contact](#contact)
+
+<!-- DEPLOYED SITE -->
+
+## Deployed Site
+
+To see the deployed site go to [mateo-wallace.github.io](https://mateo-wallace.github.io/)
+
+![example of deployed portfolio](https://github.com/Mateo-Wallace/Portfolio-React-Mateo/raw/main/src/assests/images/deployed.png)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- DEPLOYMENT INSTRUCTIONS -->
+
+## Deployment Instructions
+
+Make sure that you are on `"node": "16.x"`
+
+From the command line use:
+
+1. `npm run build`
+1. Copy `README.md` into the build folder
+1. `cd build/`
+1. 🡇
+
+```
+git init
+git add .
+git commit -m "production build"
+git remote add origin git@github.com:Mateo-Wallace/mateo-wallace.github.io.git
+git remote -v
+git push origin +main
+```
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- LICENSE -->
+
+## License
+
+Distributed under the MIT License. See [LICENSE](./LICENSE) for more information.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- CONTACT -->
+
+## Contact
+
+Mateo Wallace - [GitHub](https://github.com/Mateo-Wallace) - [Email](mailto:mateo.t.wallace@gmail.com) - [LinkedIn](https://www.linkedin.com/in/mateo-wallace/)
+
+Project Repo: [github.com/Mateo-Wallace/Portfolio-React-Mateo](https://github.com/Mateo-Wallace/Portfolio-React-Mateo)
+
+Deployed Repo: [github.com/Mateo-Wallace/mateo-wallace.github.io](https://github.com/Mateo-Wallace/mateo-wallace.github.io)
+
+Deployed Site: [mateo-wallace.github.io](https://mateo-wallace.github.io/)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
---
+++ precache-manifest.5d2ad5322b69de2176a3922b85ba3bb7.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e84ae44abfa889f6f3fb2f4bbf2b87c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72e4f3d2369902e77c7f",
+    "url": "/static/css/main.30eaed4d.chunk.css"
+  },
+  {
+    "revision": "05134abad33ea14247b1",
+    "url": "/static/js/2.ab740c8e.chunk.js"
+  },
+  {
+    "revision": "4c25d06d95384d1d9cff7a908ed8d725",
+    "url": "/static/js/2.ab740c8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72e4f3d2369902e77c7f",
+    "url": "/static/js/main.5e27f1de.chunk.js"
+  },
+  {
+    "revision": "e2938ac811ced1edf05c",
+    "url": "/static/js/runtime-main.807d4875.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "358f9df32013d61fad064b8cc9536d63",
+    "url": "/static/media/MW-Logo-No-BG.358f9df3.svg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c2cca4a8074835e209591c58870329ff",
+    "url": "/static/media/blog-boy.c2cca4a8.png"
+  },
+  {
+    "revision": "bb9bf9c0ab6c680abe6199599586ee57",
+    "url": "/static/media/book-search.bb9bf9c0.png"
+  },
+  {
+    "revision": "7b3073840f436bca39b6ebbe0d3b68fe",
+    "url": "/static/media/css-cheatsheet.7b307384.png"
+  },
+  {
+    "revision": "e3a92ef7447022c357819369e47faa04",
+    "url": "/static/media/deployed.e3a92ef7.png"
+  },
+  {
+    "revision": "ef22df664f5ff0190a216cf71a7943f9",
+    "url": "/static/media/dumbot.ef22df66.png"
+  },
+  {
+    "revision": "4edb9ebe9c2c13c5a8122dd2ec071513",
+    "url": "/static/media/edx-bootcamps-logo.4edb9ebe.jpg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "1f75902d1af2fce479a34d34e3163d3f",
+    "url": "/static/media/flappy-pappy.1f75902d.png"
+  },
+  {
+    "revision": "43a3d4d492409dc601b5b82e74aee2e1",
+    "url": "/static/media/github.43a3d4d4.png"
+  },
+  {
+    "revision": "a56e442cc4b3a9376ce79a2214590667",
+    "url": "/static/media/just-tech-news.a56e442c.png"
+  },
+  {
+    "revision": "9133dc7d45081f70bdb53b67e09099f2",
+    "url": "/static/media/mateo-logo.9133dc7d.png"
+  },
+  {
+    "revision": "35f95e63a86a846d5724234cfdba5c34",
+    "url": "/static/media/mateo-ruana.35f95e63.jpg"
+  },
+  {
+    "revision": "f6898124cbf7ea653f7c57f63562363a",
+    "url": "/static/media/mongo.f6898124.png"
+  },
+  {
+    "revision": "3ef2869b0305ebf6496f95bd7a566eb6",
+    "url": "/static/media/note-taker.3ef2869b.png"
+  },
+  {
+    "revision": "7571f06049ca4ddd545d50869ee5de36",
+    "url": "/static/media/postman.7571f060.png"
+  },
+  {
+    "revision": "d5c5173533be67fbde083beb7e61a882",
+    "url": "/static/media/rpg-dice-js.d5c51735.png"
+  },
+  {
+    "revision": "d9902406eda24cc922c1753fffdd2ef5",
+    "url": "/static/media/shocase.d9902406.png"
+  },
+  {
+    "revision": "dd8d48daa106a79f42612499611fadd0",
+    "url": "/static/media/steamy-scoops.dd8d48da.png"
+  },
+  {
+    "revision": "8a0e70b8732a8b8c4332fde1c6276379",
+    "url": "/static/media/ucf-logo.8a0e70b8.png"
+  },
+  {
+    "revision": "31d0f4661484f2ef7057852578fb9289",
+    "url": "/static/media/us-postal.31d0f466.png"
+  },
+  {
+    "revision": "aafe2dcc095a5bf7b79c799620495757",
+    "url": "/static/media/uspp-logo.aafe2dcc.png"
+  },
+  {
+    "revision": "9937f017c758a1df98ca0ae7c24f1f9a",
+    "url": "/static/media/weather-boy.9937f017.png"
+  }
+]);
